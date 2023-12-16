63e88933cc1ef70b14b510ae3126ff453e0c326c
Tru Nguyen <nguyentruhn@gmail.com>
2023-12-16 22:19:14 +0700

    xong

---
+++ main.js
@@ -0,0 +1,5 @@
+let menuMb = document.querySelector(".menuMb");
+let listMenu = document.querySelector(".menulist");
+menuMb.addEventListener("click", () => {
+    listMenu.classList.toggle("toggle");
+});
