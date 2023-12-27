48d7d7c5480b7063a6fea52d1e23bf23093bd35e
Tru Nguyen <nguyentruhn@gmail.com>
2023-12-27 12:02:32 +0700

    xong

--- main.js
+++ main.js
@@ -25,3 +25,57 @@ function backToTop() {
     lastScrollTop = scrollTop;
   }
   
+  // những con số
+  let isAnimationTriggered = false;
+  function runNumberAnimation() {
+    if (isAnimationTriggered) {
+      return;
+    }
+    let numberElements = document.getElementsByClassName('number');
+    let targets = [45, 27, 836];
+    let currentNumbers = [0, 0, 0];
+    let intervalIds = [];
+  
+    for (let i = 0; i < numberElements.length; i++) {
+        let numberElement = numberElements[i];
+        let targetNumber = targets[i];
+        let currentNumber = currentNumbers[i];
+  
+        let intervalId = setInterval(function() {
+            if (currentNumber >= targetNumber) {
+                clearInterval(intervalId);
+            } else {
+                currentNumber++;
+                numberElement.innerText = formatNumber(currentNumber, i);
+            }
+        },10);
+        
+        intervalIds.push(intervalId);
+    }
+  
+    function formatNumber(number, index) {
+        if (index === 0) {
+          return number + '+';
+        } else if (index === 1) {
+          return number + '+';
+        } else if (index === 2) {
+            return number + '+';
+        } 
+    }
+    isAnimationTriggered = true;
+    return intervalIds;
+  }
+  
+  window.addEventListener("scroll", number);
+
+  function number() {
+    var timelines = document.querySelectorAll(".number");
+    for (var i = 0; i < timelines.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = timelines[i].getBoundingClientRect().top;
+      var elementVisible = 100;
+      if (elementTop < windowHeight - elementVisible) {
+        runNumberAnimation();
+      }
+    }
+  }
